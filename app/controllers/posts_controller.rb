c10f1d46dbc4c807597d7d4af98ba86136a7ed5c
pournami <pournami1707@gmail.com>
2020-10-13 19:55:46 +0530

    Add create,store,view posts functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+
+	end
+
+	def new
+		@post = Post.new
+
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post =Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+
+	end
+
+
+private
+	def post_params
+
+		params.require(:post).permit(:title,:body,:tag)
+	end
+
+end
